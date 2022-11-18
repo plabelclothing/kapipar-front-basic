932555c66a4fe6d31655beaf9b9c7d7ec5cd2fde
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2022-11-18 19:31:19 +0300

    refactor bug with faq page

--- pages/About/faq.js
+++ pages/About/faq.js
@@ -1,8 +1,14 @@
-const accord = document.getElementsByClassName('faq-accordion');
-
-for (i = 0; i < accord.length; i++) {
-  accord[i].onclick = function () {
-    this.nextElementSibling.classList.toggle("show");
-  };
+const acc = document.getElementsByClassName("faq-accordion");
+var i;
+var len = acc.length;
+for (i = 0; i < len; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
-
