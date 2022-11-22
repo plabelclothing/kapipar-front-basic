ead3808ef4a3ec0d9bd4790365d1ecc922a152bb
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2022-11-22 13:20:37 +0300

    refactor profile input

--- pages/Profile/profile.js
+++ pages/Profile/profile.js
@@ -1,20 +1,17 @@
 const accord = document.querySelector('.edit-btn');
 const panel = document.querySelector('.panel');
+const fullAddress = document.querySelector('.full-address');
+
 const password = document.querySelector('.change-btn');
 const hideItem = document.querySelector('.panel-password');
+const passwordInput = document.querySelector('.password-input');
 
 accord.addEventListener('click', function() {
-  if(panel.style.display == 'none') {
-    panel.style.display = 'block';
-  } else {
-    panel.style.display = 'none';
-  }
-})
+  panel.classList.toggle('panel-show');
+  fullAddress.classList.toggle('address-hidden');
+});
 
 password.addEventListener('click', function() {
-  if(hideItem.style.display == 'none') {
-    hideItem.style.display = 'block';
-  } else {
-    hideItem.style.display = 'none';
-  }
-})
+  hideItem.classList.toggle('panel-password_show');
+  passwordInput.classList.toggle('password-input_hidden');
+});
