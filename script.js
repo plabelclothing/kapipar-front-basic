da69aca87ac876a5eec3576996e14b2fcd767160
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2022-12-07 17:41:01 +0300

    refactor header

--- script.js
+++ script.js
@@ -50,11 +50,25 @@ if (isMobile.any()) {
 // menu burger
 
 const iconMenu = document.querySelector('.menu-icon');
+const menuExitZone = document.querySelector('.menu-exit_zone');
 if (iconMenu) {
   const menuBurger = document.querySelector('.menu-body_wrapper');
   iconMenu.addEventListener('click', function(e) {
     document.body.classList.toggle('lock');
     iconMenu.classList.toggle('active');
     menuBurger.classList.toggle('active');
+    menuExitZone.classList.toggle('active');
+  });
+}
+
+// menu exit zone
+
+if (menuExitZone) {
+  const menuBurger = document.querySelector('.menu-body_wrapper');
+  menuExitZone.addEventListener('click', function(e) {
+    document.body.classList.remove('lock');
+    iconMenu.classList.remove('active');
+    menuBurger.classList.remove('active');
+    menuExitZone.classList.remove('active');
   });
 }
