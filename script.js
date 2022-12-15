bb3aac9537e25187955e9f2ead981dd93330f759
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2022-12-15 17:20:33 +0300

    refactor responsive first page

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const isMobile = {
     return navigator.userAgent.match(/BlackBerry/i);
   },
   iOS: function () {
-    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    return navigator.userAgent.match(/iPhone|iPod/i);
   },
   Opera: function () {
     return navigator.userAgent.match(/Opera Mini/i);
